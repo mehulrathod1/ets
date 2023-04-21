2f973fd5a975ce77474714d668581759dede99bb
Hitesh Raviya <raviyahitesh@gmail.com>
2023-04-21 09:55:38 +0530

    [+] employee manageOrder Add,delete (Done).

--- lib/Controller/EmployeeController/employee_add_order_controller.dart
+++ lib/Controller/EmployeeController/employee_add_order_controller.dart
@@ -3,10 +3,11 @@
 import 'package:etsemployee/Models/EmployeeModel/employee_add_order_model.dart';
 import 'package:etsemployee/Network/api_constant.dart';
 import 'package:etsemployee/Network/post_api_client.dart';
+import 'package:etsemployee/Screens/Contractors/ManageTask/manage_task.dart';
 import 'package:flutter/material.dart';
 
 class EmployeeAddOrderController {
-  TextEditingController sigDataUrl = TextEditingController();
+  EmployeeAddOrderModel? addOrderModel;
   TextEditingController estimateId = TextEditingController();
   TextEditingController orderStatus = TextEditingController();
   TextEditingController orderName = TextEditingController();
@@ -16,9 +17,14 @@ class EmployeeAddOrderController {
   TextEditingController startDate = TextEditingController();
   TextEditingController dueDate = TextEditingController();
 
-  Future<EmployeeAddOrderModel> addOrder(BuildContext context) async {
+  Future addOrder(BuildContext context, {required String? signature}) async {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return const Center(child: CircularProgressIndicator());
+        });
     var response = await postDataWithHeader(paramUri: ApiConstant.employeeAddOrder, params: {
-      'sig-dataUrl': sigDataUrl.text,
+      'sig-dataUrl': signature!,
       'estimate_id': estimateId.text,
       'orderstatus': orderStatus.text,
       'order_name': orderName.text,
@@ -28,13 +34,35 @@ class EmployeeAddOrderController {
       'start_date': startDate.text,
       'due_date': dueDate.text,
     });
-    var res = EmployeeAddOrderModel.fromJson(response);
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-        content: Text(res.message),
-        duration: const Duration(seconds: 2),
-      ),
-    );
-    return res;
+    debugPrint("addOrder response :- ${response.toString()}");
+    if (response["status"] == 'True') {
+      var res = EmployeeAddOrderModel.fromJson(response);
+      addOrderModel = res;
+      Navigator.pop(context);
+      Navigator.push(context, MaterialPageRoute(builder: (context) => const ManageTask()));
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(res.message),
+          duration: const Duration(seconds: 2),
+        ),
+      );
+    } else {
+      Navigator.pop(context);
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(response["message"]),
+          duration: const Duration(seconds: 2),
+        ),
+      );
+    }
+  }
+
+  Future getEstimateOrderListForEmployee(BuildContext context) async {
+    var response = await getData(paramUri: ApiConstant.employeeGetEstimateOrder);
+    if (response["status"] == "True" && response["data"] != null) {
+      return response["data"]["List"];
+    } else {
+      return null;
+    }
   }
 }
--- lib/Network/api_constant.dart
+++ lib/Network/api_constant.dart
@@ -47,6 +47,7 @@ class ApiConstant {
   static const employeeAddNote = 'employee/add-notes';
   static const employeeEditNote = 'employee/edit-note/';
   static const employeeInvoice = 'employee/view-invoice-lists';
+  static const employeeGetEstimateOrder = 'employee/get-orders-estiment';
   static const employeeAddOrder = 'employee/add-orders';
   static const employeeGetImage = 'employee/view-image-employee?date=2023-02-03';
   static const employeeGetMap = 'employee/view-map-employee?date=2023-02-03';
